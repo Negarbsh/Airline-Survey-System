ca3f66dbeb9488e18a93c81fa9daaa14492eb549
Negar Babashah <negar.babashah@tapsi.cab>
2023-01-10 11:39:17 +0330

    Init backend

---
+++ backend/src/test/kotlin/db/ailine/survey/SurveyApplicationTests.kt
@@ -0,0 +1,13 @@
+package db.ailine.survey
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SurveyApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
