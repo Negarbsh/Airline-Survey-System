249b8f1110e6e758c9061a385c21057a4df582e9
AmirHossein-nr <arazlighi@gmail.com>
2023-01-10 11:37:01 +0330

    initial frontend

---
+++ frontend/AirLineSurveys/AirLineSurveys/AirLineSurveysApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirLineSurveysApp.swift
+//  AirLineSurveys
+//
+//  Created by Amirhossein Razlighi on 10/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AirLineSurveysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
